1f1ca731b1fbffc92a83ec866ba41243e5bea8ad
이지혁 <olzlgur@naver.com>
2023-01-01 21:47:21 +0900

    initial commit

---
+++ src/main/java/Boormii/soonDelivery/SoonDeliveryApplication.java
@@ -0,0 +1,13 @@
+package Boormii.soonDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoonDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoonDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Boormii/soonDelivery/SoonDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package Boormii.soonDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoonDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
