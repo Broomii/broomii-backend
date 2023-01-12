e47454be4cb3a3bee90d0a638a740c5652ffe3df
채희찬 <eqholic1125@gmail.com>
2023-01-12 22:20:05 +0900

    feat: 닉네임 중복 검증 api 구현

--- src/main/java/Boormii/soonDelivery/member/controller/MemberController.java
+++ src/main/java/Boormii/soonDelivery/member/controller/MemberController.java
@@ -4,9 +4,7 @@ import Boormii.soonDelivery.member.domain.Member;
 import Boormii.soonDelivery.member.dto.JoinRequestDto;
 import Boormii.soonDelivery.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +16,24 @@ public class MemberController {
     {
         Member member = Member.registerMember(joinRequestDto);
         return memberService.join(member);
+    }
+
+//    이메일로 인증번호 전송
+    @GetMapping("/api/sendCertification/{email}")
+    public void validateDuplicatedId(@PathVariable("email") String email)
+    {
+        memberService.validateDuplicateEmail(email);
+    }
+
+//    인증번호 체크
+
+
+//    닉네임 중복 검증
+    @GetMapping("/api/checkNickname/{nickname}")
+    public String validateDuplicatedNickname(@PathVariable("nickname") String nickname)
+    {
+        return memberService.validateDuplicateNickName(nickname);
+
     }
 
 }
