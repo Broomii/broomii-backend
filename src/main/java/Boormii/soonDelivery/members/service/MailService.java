15c22bc6e375ef6c61e89a6a01da6dd22f682b96
이지혁 <olzlgur@naver.com>
2023-01-15 17:33:23 +0900

    feat: 이메일 인증 메일 전송 api 구현

---
+++ src/main/java/Boormii/soonDelivery/members/service/MailService.java
@@ -0,0 +1,47 @@
+package Boormii.soonDelivery.members.service;
+
+import Boormii.soonDelivery.members.utils.MailMessage;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+    private final JavaMailSender javaMailSender;
+
+    private static final String from_address = "olzlgur@naver.com";
+    private static final String text = "이메일 인증 코드";
+
+    public String mailSend(String email) {
+        String certificationKey = createKey();
+        try {
+            MailMessage mailMessage = new MailMessage(javaMailSender);
+            mailMessage.setSubject(text);
+            mailMessage.setFrom(MailService.from_address, "Broomii");
+            mailMessage.setTo(email);
+
+            String message = mailMessage.emailDuplicateCheckMsgForm(email, certificationKey);
+
+            mailMessage.setText(message);
+            mailMessage.send();
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return "메일 전송 완료";
+    }
+
+    private String createKey(){
+        Random random = new Random();
+        return String.valueOf(random.nextInt(900000) + 100000);
+    }
+
+}
