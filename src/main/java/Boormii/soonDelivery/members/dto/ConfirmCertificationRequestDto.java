e75bf5d66edca5b594dbe5cc7407a24bd4079926
채희찬 <eqholic1125@gmail.com>
2023-01-19 22:14:29 +0900

    feat: 인증 번호 확인 api 구현

---
+++ src/main/java/Boormii/soonDelivery/members/dto/ConfirmCertificationRequestDto.java
@@ -0,0 +1,12 @@
+package Boormii.soonDelivery.members.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ConfirmCertificationRequestDto {
+
+    private String email;
+
+    private String certification;
+
+}
