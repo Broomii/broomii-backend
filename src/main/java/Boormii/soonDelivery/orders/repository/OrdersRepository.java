27c88ebcc340e8ef9f6f2e3fc019846171a5bec5
이지혁 <olzlgur@naver.com>
2023-05-29 13:40:32 +0900

    feat: 시간 순으로 정렬된 주문 목록 조회 메서드 구현

--- src/main/java/Boormii/soonDelivery/orders/repository/OrdersRepository.java
+++ src/main/java/Boormii/soonDelivery/orders/repository/OrdersRepository.java
@@ -4,6 +4,7 @@ import Boormii.soonDelivery.orders.domain.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,6 +14,8 @@ public interface OrdersRepository extends JpaRepository<Orders, Long> {
     void deleteById(Long id);
 
     Optional<Orders> findById(Long id);
+
+    List<Orders> findAllByOrderByCreateTime();
 
     
 }
