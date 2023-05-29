ccd8315a05dd35089c191736136ba66f5e5564cc
이지혁 <olzlgur@naver.com>
2023-05-29 13:38:52 +0900

    fix: 마이페이지 조회시 이름, 주소 반환

--- src/main/java/Boormii/soonDelivery/members/dto/MyPageResponseDto.java
+++ src/main/java/Boormii/soonDelivery/members/dto/MyPageResponseDto.java
@@ -1,6 +1,7 @@
 package Boormii.soonDelivery.members.dto;
 
 import Boormii.soonDelivery.members.domain.Members;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -10,13 +11,26 @@ public class MyPageResponseDto {
     @NotNull
     private String nickName;
 
-    @NotNull
+    @Nullable
     private String department;
 
-    public static MyPageResponseDto register(Members members){
+    @NotNull
+    private String name;
+
+    @Nullable
+    private String defaultDeliveryAddress;
+
+    public static MyPageResponseDto register(Members members) {
         MyPageResponseDto myPageResponseDto = new MyPageResponseDto();
         myPageResponseDto.setNickName(members.getNickName());
-        myPageResponseDto.setDepartment(members.getDepartment());
+
+        if (members.getDepartment() != null) {
+            myPageResponseDto.setDepartment(members.getDepartment());
+        }
+
+        if (members.getName() != null) {
+            myPageResponseDto.setName(members.getName());
+        }
 
         return myPageResponseDto;
     }
